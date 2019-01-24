963e5aefae3796498b0fa182a30b2a783f6e87f6
ziazi <ziaziek@poczta.fm>
2019-01-24 11:02:45 +0100

    Main class changed to use new Guesser approach.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
-        new Guesser(new Scanner(System.in));
+        new Guesser(new Scanner(System.in)).run();
     }
 }
